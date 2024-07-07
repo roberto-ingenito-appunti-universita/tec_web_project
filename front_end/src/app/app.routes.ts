7f04527a16f171f979e8cfa98ce9ef7f2d16416e
roberto ingenito <ingenitoroby@gmail.com>
2024-07-07 17:22:36 +0200

    front end first release

---
+++ front_end/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+
+export const routes: Routes = [
+    { path: "signin", component: SigninComponent },
+    { path: "signup", component: SignupComponent },
+    { path: '**', component: PageNotFoundComponent },
+];
