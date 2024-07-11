4a1f38a66936219e1fb023f570bd9ed1a5051a64
roberto ingenito <ingenitoroby@gmail.com>
2024-07-11 20:32:09 +0200

    spostata la logica di idea_router in idea_controller

--- back_end/src/controllers/idea_controller.js
+++ back_end/src/controllers/idea_controller.js
@@ -1,4 +1,32 @@
-import { Idea } from '../models/model_config.js'
+import { QueryTypes } from 'sequelize';
+import connection from '../config/db_connection.js';
 
 export default class IdeaController {
+
+    // carica 500 idee
+    // restituisce un array di oggetti
+    async getIdeas() {
+        return await connection.query(`
+            WITH ideas AS (
+                SELECT 
+                    i.*,
+                    (SELECT count(*) FROM public.vote as v WHERE v."ideaFK" = i.id AND v."isUpvote" = true) as up_vote_quantity,
+                    (SELECT count(*) FROM public.vote as v WHERE v."ideaFK" = i.id AND v."isUpvote" = false) as down_vote_quantity,
+                    (SELECT count(*) FROM public.comment as c WHERE c."ideaFK" = i.id) as comment_quantity
+                FROM 
+                    public.idea as i
+            )
+            SELECT *
+            FROM ideas
+            ORDER BY 
+                (up_vote_quantity - down_vote_quantity) DESC,
+                (up_vote_quantity + down_vote_quantity) DESC,
+                "createdAt" DESC,
+                id DESC
+            LIMIT 500;
+        `,
+            { type: QueryTypes.SELECT }
+        );
+
+    }
 }
--- back_end/src/routes/api/v1/idea_router.js
+++ back_end/src/routes/api/v1/idea_router.js
@@ -1,41 +1,19 @@
 import express from 'express';
-import { Idea } from '../../../models/model_config.js'
 import AuthController from '../../../controllers/auth_controller.js';
-import connection from '../../../config/db_connection.js';
-import { QueryTypes } from 'sequelize';
+import IdeaController from '../../../controllers/idea_controller.js';
 
 const ideaRouter = express.Router();
 
 const authController = new AuthController();
+const ideaController = new IdeaController();
 
 ideaRouter.get(
     '/api/v1/ideas',
     authController.authenticateToken,
     async (req, res, next) => {
         try {
-            const result = await connection.query(`
-                    WITH ideas AS (
-                        SELECT 
-                            i.*,
-                            (SELECT count(*) FROM public.vote as v WHERE v."ideaFK" = i.id AND v."isUpvote" = true) as up_vote_quantity,
-                            (SELECT count(*) FROM public.vote as v WHERE v."ideaFK" = i.id AND v."isUpvote" = false) as down_vote_quantity,
-                            (SELECT count(*) FROM public.comment as c WHERE c."ideaFK" = i.id) as comment_quantity
-                        FROM 
-                            public.idea as i
-                    )
-                    SELECT *
-                    FROM ideas
-                    ORDER BY 
-                        (up_vote_quantity - down_vote_quantity) DESC,
-                        (up_vote_quantity + down_vote_quantity) DESC,
-                        "createdAt" DESC,
-                        id DESC
-                    LIMIT 5000;
-                `,
-                { type: QueryTypes.SELECT }
-            );
-
-            res.status(200).json(result);
+            const ideas = await ideaController.getIdeas();
+            res.status(200).json(ideas);
         } catch (error) {
             res.status(404).send(error);
         }
