34ecdd2b8a476d3f01527be483c0840db83ba46d
roberto ingenito <ingenitoroby@gmail.com>
2024-07-13 16:42:17 +0200

    add navbar user popup

--- front_end/src/app/components/nav-bar/nav-bar.component.ts
+++ front_end/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -8,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  isUserPopupOpen = false;
+  authService = inject(AuthService);
 
+  signOut() { this.authService.signOut(); }
+
+  toggleUserPopup(event: MouseEvent): void {
+    this.isUserPopupOpen = !this.isUserPopupOpen;
+    event.stopPropagation(); // Previene la chiusura immediata
+  }
+
+  preventClose(event: MouseEvent): void {
+    event.stopPropagation(); // Previene la chiusura quando si clicca all'interno della finestra
+  }
+
+  @HostListener('document:click', ['$event'])
+  closePopup(event: MouseEvent): void {
+    if (this.isUserPopupOpen) {
+      this.isUserPopupOpen = false;
+    }
+  }
 }
