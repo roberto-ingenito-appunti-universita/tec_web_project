215b4a63cce734f5b28ca27e8c121f01c65c7eaa
roberto ingenito <ingenitoroby@gmail.com>
2024-09-06 19:58:20 +0200

    test hosted db

--- back_end/src/config/db_connection.js
+++ back_end/src/config/db_connection.js
@@ -1,10 +1,10 @@
 import { Sequelize } from "sequelize";
 
 const connection = new Sequelize({
-    database: "postgres",
-    username: "postgres",
-    password: "admin",
-    host: "localhost",
+    database: "postgres_mq4d",
+    username: "postgres_mq4d_user",
+    password: "Bz2QCoPBRMth6UPwZjFs2AbWXDpBNDny",
+    host: "dpg-crdk1h08fa8c738d1n2g-a",
     port: 5432,
     schema: "public",
     dialect: "postgres",
