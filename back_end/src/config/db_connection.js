be24845bf09662b9151e6e7cdb03b924f9bc51c2
roberto ingenito <ingenitoroby@gmail.com>
2024-09-06 20:14:10 +0200

    test hosted db

--- back_end/src/config/db_connection.js
+++ back_end/src/config/db_connection.js
@@ -1,10 +1,10 @@
 import { Sequelize } from "sequelize";
 
 const connection = new Sequelize({
-    database: "postgres_mq4d",
-    username: "postgres_mq4d_user",
-    password: "Bz2QCoPBRMth6UPwZjFs2AbWXDpBNDny",
-    host: "dpg-crdk1h08fa8c738d1n2g-a",
+    database: "postgres",
+    username: "postgres.ijlttpzikglpuvnfqzcn",
+    password: "NqcKX2K2OjDpNGtl",
+    host: "aws-0-eu-central-1.pooler.supabase.com",
     port: 5432,
     schema: "public",
     dialect: "postgres",
