e117fe358455cd859b88dd86fd72b4690273dfd8
roberto ingenito <ingenitoroby@gmail.com>
2024-09-07 13:52:55 +0200

    modificato cors backend

--- back_end/src/index.js
+++ back_end/src/index.js
@@ -13,8 +13,12 @@ dotenv.config()
 const PORT = process.env.PORT || 3000;
 const app = express()
 
-app.use(cors())
-app.use(express.json())
+app.use(cors({
+    origin: 'https://tec-web-project-frontend.onrender.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true // If you're using cookies or authentication
+  }));
+app.use(express.json());
 
 /* routes */
 app.use(userRouter);
