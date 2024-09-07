d467036d3af1f2da3cb433d5b055ef6be0ca57c7
roberto ingenito <ingenitoroby@gmail.com>
2024-09-07 13:22:19 +0200

    update apiurl in frontend

--- front_end/src/app/services/backend.service.ts
+++ front_end/src/app/services/backend.service.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class BackendService {
-    public apiUrl = 'http://localhost:3000/api/v1';
+    public apiUrl = 'https://tec-web-project.onrender.com/api/v1';
 }
